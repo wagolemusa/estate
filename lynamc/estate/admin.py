111d2c1337dc28e4588a89a9ab15e88bf8efe1c2
kisumutalent <homiemusa@gmail.com>
2019-08-13 10:40:47 +0300

    Castomize admin area

--- lynamc/estate/admin.py
+++ lynamc/estate/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ["title", "updated", "timestamp"]
+	list_display_links = ["updated", "timestamp"]
+	list_editable = ["title"]
+	list_filter = ["updated", "timestamp"]
+	search_fields = ["title", "content"]
+	
+	class Meta:
+		model = Post
+
+# register site
+admin.site.register(Post, PostModelAdmin)
+
+
+
